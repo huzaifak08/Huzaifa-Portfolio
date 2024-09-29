0df6930fbe1c17e593f7b2bc985093c348bda787
huzaifak08 <hk7928042@gmail.com>
2024-09-29 19:40:56 +0500

    Final Release

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
         <h1 className={styles.title}>Hi, I'm Huzaifa</h1>
         <p className={styles.description}>
           I'm a full stack Flutter developer with almost 1.5 years of
-          Professional experience.I'm a BS Computer Science Graduate from
+          Professional experience. I'm a BS Computer Science Graduate from
           Comsats University. I've strong problem solving skills and love to
           learn new things in my field and currently learning SwiftUI.
         </p>
